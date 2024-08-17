080edcf797eac0433d8610f79c69597e8cf6f739
Robin <97387960+robeecodes@users.noreply.github.com>
2024-08-17 17:18:13 +0100

    fix persistence

--- Assets/Scripts/Cutout/Cutout.cs
+++ Assets/Scripts/Cutout/Cutout.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cutout : MonoBehaviour {
     private DrawWithMouse _drawing;
@@ -10,9 +11,14 @@ public class Cutout : MonoBehaviour {
     private int _score;
 
     private void Start() {
+        if (PersistenceManager.Instance.cutoutMesh != null) {
+            _complete = true;
+        }
         _drawing = GetComponent<DrawWithMouse>();
         _polygon = GetComponent<CreatePolygon>();
         _checkAccuracy = GetComponent<CheckAccuracy>();
+        
+        
     }
 
     private void Update() {
@@ -20,6 +26,7 @@ public class Cutout : MonoBehaviour {
             _polygon.Build();
             _score = _checkAccuracy.CalculateScore();
             _complete = true;
+            SceneManager.LoadScene("DressUpGame", LoadSceneMode.Single);
         }
     }
 }
